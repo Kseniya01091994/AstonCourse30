c34c427c53ad9fa0e2d5b24c05fa58f9cc5cabe6
Maxim Lugvin <maximlugvin@gmail.com>
2025-12-19 18:56:08 +0300

    add new branch with lessonThree

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,11 +6,8 @@ public class Main {
         ProductManager.createAndDisplayProducts();
 
         System.out.println("\n=== Проверка класса Park ===\n");
-
         Park park = new Park();
-
         Park.Attraction attraction1 = park.new Attraction("Карусель", "09:00 - 21:00", 300);
-
         System.out.println("Аттракцион 1:");
         attraction1.printInfo();
         System.out.println();
