34a02af0eb6832b946172c72d59d99ee08f04635
Maxim Lugvin <maximlugvin@gmail.com>
2025-12-19 18:05:32 +0300

    add new branch with lessonTwo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,28 +2,8 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        Product[] productsArray = new Product[5];
-
-        productsArray[0] = new Product("Samsung S25 Ultra", "01.02.2025",
-                "Samsung Corp.", "Korea", 5599, true);
-
-        productsArray[1] = new Product("iPhone 15 Pro Max", "15.09.2024",
-                "Apple Inc.", "USA", 6499, false);
-
-        productsArray[2] = new Product("Xiaomi 14 Pro", "20.11.2024",
-                "Xiaomi Corporation", "China", 3299, true);
-
-        productsArray[3] = new Product("Samsung A11", "04.10.2023",
-                "Samsung Corp.", "Korea", 3499, false);
-
-        productsArray[4] = new Product("iPhone 16", "11.02.2025",
-                "Apple Inc.", "USA", 7899, false);
-
-
-        for (int i = 0; i < productsArray.length; i++) {
-            System.out.println("Товар " + (i + 1) + ":");
-            productsArray[i].printInfo();
-        }
+        // Создание и вывод массива из 5 товаров
+        ProductManager.createAndDisplayProducts();
 
         System.out.println("\n=== Проверка класса Park ===\n");
 
---
+++ src/main/java/org/example/ProductManager.java
@@ -0,0 +1,30 @@
+package org.example;
+
+public class ProductManager {
+    public static void createAndDisplayProducts() {
+        // Создаем массив из 5 товаров
+        Product[] productsArray = new Product[5];
+
+        productsArray[0] = new Product("Samsung S25 Ultra", "01.02.2025",
+                "Samsung Corp.", "Korea", 25599, true);
+
+        productsArray[1] = new Product("iPhone 15 Pro Max", "15.09.2024",
+                "Apple Inc.", "USA", 66499, false);
+
+        productsArray[2] = new Product("Xiaomi 14 Pro", "20.11.2024",
+                "Xiaomi Corporation", "China", 33299, true);
+
+        productsArray[3] = new Product("Samsung A11", "04.10.2023",
+                "Samsung Corp.", "Korea", 23499, false);
+
+        productsArray[4] = new Product("iPhone 16", "11.02.2025",
+                "Apple Inc.", "USA", 77899, false);
+
+        // Выводим информацию о каждом товаре
+        for (int i = 0; i < productsArray.length; i++) {
+            System.out.println("Товар " + (i + 1) + ":");
+            productsArray[i].printInfo();
+        }
+    }
+}
+
